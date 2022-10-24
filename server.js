821d85615f35a8863f096867b420ef7277c50cc7
Austin Moore <austin1moore@gmail.com>
2022-10-24 15:59:11 -0500

    got everything working

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 const express = require("express");
-const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 const db = require("./db/db.json");
 const { v4: uuidv4 } = require('uuid');;
-const { readFromFile, readAndAppend, readAndDelete } = require('./routes/readWriteFiles');
-
+const { readFromFile, readAndAppend, readAndDelete } = require('./routes/readWrite');
+const htmlroutes = require("./routes/htmlroutes")
 // Middleware
+const app = express();
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-})
-
 app.get("/api/notes", (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
-
-
 
 app.post("/api/notes", function (req, res) {
   console.log(req.body);
@@ -44,5 +38,8 @@ app.delete("/api/notes/:id", function (req, res) {
     readAndDelete('./db/db.json', req.params.id);
     res.json(`deletion successful`);
 });
+
+app.use("/",htmlroutes)
+
 
 app.listen(PORT, () => console.log("listening on port http://localhost:" + PORT))
